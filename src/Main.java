6136ccc6005c94d112ee61110290817d2ace767d
Waleed Mohamed <waleedm11@nycstudents.net>
2025-09-25 08:36:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the tip calculator, how much is the price without tip? (No symbols)");
+        double basePrice = scan.nextDouble();
+        scan.nextLine();
+        System.out.println("What about how much percent you want to tip?");
+        double tip = (scan.nextDouble() * basePrice) / 100;
+        scan.nextLine();
+        System.out.println("And how many people are there in your group?");
+        int people = scan.nextInt();
+        scan.nextLine();
+        double total = basePrice + tip;
+        System.out.println();
+        System.out.println("---------------------------------------");
+        System.out.println("| Total tip: $" + String.format("%.2f", tip) + " |");
+        System.out.println("---------------------------------------");
+        System.out.println("| Total price: $" + String.format("%.2f", total) + " |");
+        System.out.println("---------------------------------------");
+        System.out.println("| Total bill per person: $" + String.format("%.2f", total / people) + " |");
+        System.out.println("---------------------------------------");
+        System.out.println("| Total tip per person: $" + String.format("%.2f", tip / people) + " |");
+        System.out.println("---------------------------------------");
+    }
+}
