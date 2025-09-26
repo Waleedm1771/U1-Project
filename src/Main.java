d9729cbb30967e01f35c92bd28c154d8dfaeca70
WaleedMohamed <email@email>
2025-09-26 08:27:53 -0400

    adding some comments.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner; //imported scanner tool
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-        // getting user input
+        // getting user input and values
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the tip calculator, how much is the price without tip? (No symbols)");
         double basePrice = scan.nextDouble();
         scan.nextLine();
+
         System.out.println("What about how much percent you want to tip?");
         double tip = (scan.nextDouble() * basePrice) / 100;
         scan.nextLine();
+
         System.out.println("And how many people are there in your group?");
         int people = scan.nextInt();
         scan.nextLine();
 
+        // printing all the values out organized with spacing.
         double total = basePrice + tip;
         System.out.println();
         System.out.println("---------------------------------------");
