6f42148c3b1a406118c722de2586a02262c06c42
WaleedMohamed <email@email>
2025-09-26 08:25:46 -0400

    adding some comments.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main
 
     public static void main(String[] args)
     {
+        // getting user input
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the tip calculator, how much is the price without tip? (No symbols)");
         double basePrice = scan.nextDouble();
@@ -15,16 +16,17 @@ public class Main
         System.out.println("And how many people are there in your group?");
         int people = scan.nextInt();
         scan.nextLine();
+
         double total = basePrice + tip;
         System.out.println();
         System.out.println("---------------------------------------");
-        System.out.println("| Total tip: $" + String.format("%.2f", tip) + " |");
+        System.out.println("| Total tip: $" + String.format("%.2f", tip) + "                  |");
         System.out.println("---------------------------------------");
-        System.out.println("| Total price: $" + String.format("%.2f", total) + " |");
+        System.out.println("| Total price: $" + String.format("%.2f", total) + "               |");
         System.out.println("---------------------------------------");
-        System.out.println("| Total bill per person: $" + String.format("%.2f", total / people) + " |");
+        System.out.println("| Total bill per person: $" + String.format("%.2f", total / people) + "      |");
         System.out.println("---------------------------------------");
-        System.out.println("| Total tip per person: $" + String.format("%.2f", tip / people) + " |");
+        System.out.println("| Total tip per person: $" + String.format("%.2f", tip / people) + "        |");
         System.out.println("---------------------------------------");
     }
 }
